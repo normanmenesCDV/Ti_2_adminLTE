f3aac5f11464778df954cd9b93a6277c22e5d5a9
normanmenesCDV <nmenes@edu.cdv.pl>
2023-05-29 18:54:34 +0200

    zmiana kolejności INSERT user przy rejestracji

--- scripts/register4.php
+++ scripts/register4.php
@@ -60,10 +60,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     require_once "./connect.php";
     
-    $stmt = $conn->prepare("INSERT INTO users (email, city_id, firstName, lastName, birthday, password, created_at) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
+    $stmt = $conn->prepare("INSERT INTO `users` (`email`, `password`, `firstName`, `lastName`, `birthday`, `city_id`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
 
     $pwd_hashed = password_hash($_POST["pass"], PASSWORD_ARGON2ID); #hashowanie hasła
-    $stmt->bind_param('sissss', $_POST["email"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $pwd_hashed);
+    $stmt->bind_param('sssssi', $_POST["email"], $pwd_hashed, $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["city_id"]);
 
     $stmt->execute();
 
